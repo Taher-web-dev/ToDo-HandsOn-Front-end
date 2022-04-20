ec7bc8caef259c6d4eff03adb4e809a8f365a936
Taher <ttaherhagui@gmail.com>
2022-04-20 18:45:34 +0100

    create alerte state.

---
+++ src/Redux/alert/alert.js
@@ -0,0 +1,23 @@
+const ADD_ALERT = 'TODO/ALERT/ADD';
+const RESET_ALERT = 'TODO/ALERT/RESET';
+
+export const addAlert = (payload) =>({
+  type: ADD_ALERT,
+  payload,
+});
+
+export const resetAlert = () => ({
+  type: RESET_ALERT,
+});
+
+export const alertReducer = (state='', action) => {
+  switch (action.type) {
+    case ADD_ALERT:
+      return payload;
+    case RESET_ALERT:
+      return '';
+    default:
+      return state;
+  }
+};
+
