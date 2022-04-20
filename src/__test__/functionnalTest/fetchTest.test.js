db99a1f30b57f55127f827f7120eff333077d8a7
Taher <ttaherhagui@gmail.com>
2022-04-20 22:29:23 +0100

    Add test for updateTask.

---
+++ src/__test__/functionnalTest/fetchTest.test.js
@@ -0,0 +1,13 @@
+import { updateTask } from "../../components/item/itemhelper";
+describe('update task with given data', () => {
+  test('update Task given correct data', async() => {
+    const id = 1;
+    const task = {
+      done: true,
+    };
+    const resp = await updateTask(id, task);
+    const result = await resp.json();
+    const { status } = result;
+    expect(status).toBe('SUCCESS');
+  });
+});
