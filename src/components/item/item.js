54d4e38e887dd56f083a5395ec73bace85b48f35
Taher <ttaherhagui@gmail.com>
2022-04-21 11:38:48 +0100

    upgrade editTask function.

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -25,13 +25,36 @@ const Item = (props) => {
       !checked ? task.style.textDecoration = 'line-through 3px rgb(0, 0, 255)' : task.style.textDecoration='none';
       dispatch(addAlert(message));
     }
-
   }
+  const editTask = (e) => {
+    e.target.style.display = 'none';
+    const deleteIcon = document.querySelector('.delete-icon');
+    deleteIcon.style.display = 'block';
+    const taskDescription = document.querySelector('.task-desc');
+    taskDescription.disabled = false;
+    taskDescription.addEventListener('change', (event) => {
+      const elem = event.target;
+      elem.disabled = true;
+      const description = elem.value;
+      const task = {
+        description,
+      };
+      const resp = await updateTask(id, task);
+      const resp = await resp.json();
+      const { status, message } = resp;
+      e.target.display = 'block';
+      deleteIcon.style.display = 'none';
+      if (status === 'Error'){
+        // dispatch action to update tasks from back-end
+        dispatch(alert(message));
+      };
+    });
+  };
     return(
       <div className="item-wrapper">
         <Checkbox {...label} Checked={done} className="ckeck-icon" onChange={taskFulfilled}/>
         <input disabled style={{ textDecoration: done ? 'line-through 3px rgb(0, 0, 255)': 'none'}} className="task-desc">{description}</input>
-        <MoreVertIcon className="edit-icon"/>
+        <MoreVertIcon className="edit-icon" onClick={} />
         <DeleteIcon style={{ display: 'none' }} className="delete-icon"/>
       </div>
     );
