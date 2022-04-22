2e546bd758449d58fd7e66d86ab6a11ef14253aa
Taher <ttaherhagui@gmail.com>
2022-04-22 14:55:21 +0100

    add content to app component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Typography } from '@mui/material';
 import Todo from './components/todo/todo';
 import { thunkTasks } from './Redux/tasks/thunk/thunk';
 
@@ -7,8 +8,14 @@ const App = () => {
   const dispatch = useDispatch();
   useEffect(() => dispatch(thunkTasks()), []);
   return (
-    <div>
+    <div className="app-wrapper">
       <Todo />
+      <div className="app-description">
+        <Typography variant="h1" className="app-title"> Todo List</Typography>
+        <Typography variant="body1" className="app-desc">
+          Create your todo list, bring things on schedule
+        </Typography>
+      </div>
     </div>
   );
 };
