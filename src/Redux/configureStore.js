9880305102864cd8c827f2b8448c5541a9dbf0b5
Taher <ttaherhagui@gmail.com>
2022-04-23 18:48:48 +0100

    configurate item on the store.

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { alertReducer } from './alert/alert';
 import tasksReducer from './tasks/reducers/reducers';
+import { itemReducer } from './item/item';
 
 const reducer = combineReducers({
   alert: alertReducer,
   tasks: tasksReducer,
+  item: itemReducer,
 });
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
