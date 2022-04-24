4ddcb16b42d3051ee7e255e19ad87288c35c25c8
Taher <ttaherhagui@gmail.com>
2022-04-24 17:39:11 +0100

    add test for header component.

---
+++ src/__test__/componentsTest/header.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Header from '../../components/header/header';
+
+const mockStore = configureStore([]);
+describe('Render correctly addItem component', () => {
+  let store;
+  let tree;
+  beforeEach(() => {
+    store = mockStore({
+      alert: null,
+      tasks: {},
+      item: 'display',
+    });
+    store.dispatch = jest.fn();
+    tree = render(
+      <Provider store={store}>
+        <Header total={0} done={0} />
+      </Provider>
+    );
+  });
+  it('should render with given state from redux store', () => {
+    expect(tree).toMatchSnapshot();
+  });
+});
